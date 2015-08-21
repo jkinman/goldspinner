e9166db6f339f0d85148b3b55b696939cca891f8
Joel Kinman <joel.kinman@gmail.com>
2015-08-21 16:31:04 -0700

    tweaks to 3 card client

--- client/app/threecardpoker/threecardpoker.controller.js
+++ client/app/threecardpoker/threecardpoker.controller.js
@@ -3,7 +3,7 @@
 angular.module('pkerApp')
   .controller('ThreecardpokerCtrl', function ($scope, threecardpoker) {
     $scope.message = 'Hello';
-
+    $scope.gameState = threecardpoker.game;
     $scope.startGame = function() {
     	
     };
--- client/app/threecardpoker/threecardpoker.service.js
+++ client/app/threecardpoker/threecardpoker.service.js
@@ -1,6 +1,14 @@
 'use strict';
 
 angular.module('pkerApp')
-  .service('threecardpoker', function () {
+  .service('threecardpoker', function ( $http, socket ) {
     // AngularJS will instantiate a singleton by calling "new" on this function
+
+    var game = [];
+
+    $http.get('/api/threecardpokers').success(function(state) {
+      game = state;
+      socket.syncUpdates('game', game);
+    });
+
   });
