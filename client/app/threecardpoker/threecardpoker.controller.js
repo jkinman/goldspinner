5b70b82e80d4cd62536df3c3b1203c00e16e2d18
Joel Kinman <joel.kinman@gmail.com>
2015-08-21 17:38:43 -0700

    incremental tests

--- client/app/threecardpoker/threecardpoker.controller.js
+++ client/app/threecardpoker/threecardpoker.controller.js
@@ -2,10 +2,11 @@
 
 angular.module('pkerApp')
   .controller('ThreecardpokerCtrl', function ($scope, threecardpoker) {
-    $scope.message = 'Hello';
+    
     $scope.gameState = threecardpoker.game;
+    
     $scope.startGame = function() {
-    	
+		    	
     };
 
 
--- client/app/threecardpoker/threecardpoker.service.js
+++ client/app/threecardpoker/threecardpoker.service.js
@@ -3,12 +3,25 @@
 angular.module('pkerApp')
   .service('threecardpoker', function ( $http, socket ) {
     // AngularJS will instantiate a singleton by calling "new" on this function
-
-    var game = [];
-
-    $http.get('/api/threecardpokers').success(function(state) {
-      game = state;
-      socket.syncUpdates('game', game);
+    this.req = $http.get('/api/threecardpokers').then( function(data, status, headers, config) {
+      // game = data;
+      // socket.syncUpdates('game', this.game);
+      return data;
     });
+
+
+
+    this.getGameData = function( ) {
+    	return $http.get('/api/threecardpokers').then( function(data, status, headers, config) {
+      // game = data;
+      // socket.syncUpdates('game', this.game);
+      return data;
+    });
+    	// return ['a', 'b'];
+    };
+
+
+    this.game = this.getGameData();
+
 
   });
