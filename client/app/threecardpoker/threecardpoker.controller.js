88e562cad6bbf149ac7aaaa66d6e8caa444ec22e
Joel Kinman <joel.kinman@gmail.com>
2015-08-25 02:06:41 -0700

    touch

--- client/app/threecardpoker/threecardpoker.controller.js
+++ client/app/threecardpoker/threecardpoker.controller.js
@@ -3,6 +3,7 @@
 angular.module('pkerApp')
   .controller('ThreecardpokerCtrl', function ($scope, threecardpoker) {
 
+
   	$scope.model = threecardpoker;
     $scope.gameService = threecardpoker.game;
     $scope.gameState = threecardpoker.game;
