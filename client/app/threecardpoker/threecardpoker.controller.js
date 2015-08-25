90463ad9ca648bfc59142a3c52a22ff911a3b36b
Joel Kinman <joel.kinman@gmail.com>
2015-08-24 22:11:16 -0700

    some changes to the poker api

--- client/app/threecardpoker/threecardpoker.controller.js
+++ client/app/threecardpoker/threecardpoker.controller.js
@@ -2,11 +2,12 @@
 
 angular.module('pkerApp')
   .controller('ThreecardpokerCtrl', function ($scope, threecardpoker) {
-    
+
+  	$scope.model = threecardpoker;
+    $scope.gameService = threecardpoker.game;
     $scope.gameState = threecardpoker.game;
-    
     $scope.startGame = function() {
-		    	
+    	
     };
 
   });
--- client/app/threecardpoker/threecardpoker.service.js
+++ client/app/threecardpoker/threecardpoker.service.js
@@ -1,26 +1,18 @@
 'use strict';
 
 angular.module('pkerApp')
-  .service('threecardpoker', function ( $http, socket ) {
+  .factory('threecardpoker', function ( $http, socket ) {
     // AngularJS will instantiate a singleton by calling "new" on this function
-    this.req = $http.get('/api/threecardpokers').then( function(data, status, headers, config) {
-      // game = data;
-      // socket.syncUpdates('game', this.game);
-      return data;
-    });
 
+    var service = {req:{}, game:{}};
+    service.newGame = function() {
+    	
+    }
 
+    service.req = $http.get('/api/threecardpokers').then( function(retval) {
+		service.game = retval.data;
+	});
 
-    this.getGameData = function( ) {
-   		$http.get('/api/threecardpokers').then( function(data, status, headers, config) {
-      		// game = data;
-      		// socket.syncUpdates('game', this.game);
-      		// return data;
-    	});
-    	return ['a', 'b'];
-    };
-
-    this.game = this.getGameData();
-
+	return service;
 
   });
