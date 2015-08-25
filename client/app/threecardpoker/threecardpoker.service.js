88d8fea80b76d4325af2582437afe31917b823c1
Joel Kinman <joel.kinman@gmail.com>
2015-08-25 01:11:56 -0700

    removing JS from template

--- client/app/threecardpoker/threecardpoker.service.js
+++ client/app/threecardpoker/threecardpoker.service.js
@@ -1,13 +1,13 @@
 'use strict';
 
 angular.module('pkerApp')
-  .factory('threecardpoker', function ( $http, socket ) {
+  .factory('threecardpoker', function ( $http ) {
     // AngularJS will instantiate a singleton by calling "new" on this function
 
     var service = {req:{}, game:{}};
     service.newGame = function() {
-    	
-    }
+
+    };
 
     service.req = $http.get('/api/threecardpokers').then( function(retval) {
 		service.game = retval.data;
