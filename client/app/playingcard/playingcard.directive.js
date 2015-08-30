8930efe49f8913387ec01fb1a33308074806b872
Joel Kinman <joel.kinman@gmail.com>
2015-08-30 00:45:44 -0700

    poker frontend update

--- client/app/playingcard/playingcard.directive.js
+++ client/app/playingcard/playingcard.directive.js
@@ -13,20 +13,6 @@ angular.module('pkerApp')
       },
       link: function (scope, el, attrs) {
 
-      	// determine suit
-      	// if( -1 > scope.suit.toLowerCase().indexOf( 'dia') ){
-      	// 	scope.suit = 'diamonds';
-      	// }
-      	// else if( -1 > scope.suit.toLowerCase().indexOf( 'club') ){
-      	// 	scope.suit = 'clubs';
-      	// }
-      	// else if( -1 > scope.suit.toLowerCase().indexOf( 'heart') ){
-      	// 	scope.suit = 'hearts';
-      	// }
-      	// else{
-      	// 	scope.suit = 'spades';
-      	// }
-
       	var card = $window.Poker.getCardImage(scope.size, scope.suit, scope.value.toLowerCase());
       	el.append( card );
 
